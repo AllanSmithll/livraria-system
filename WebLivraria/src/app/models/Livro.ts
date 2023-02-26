5a4014552083704ea9cf86df877608cd04d3859c
allansmithll <allanammancio2004@gmail.com>
2023-02-26 20:33:08 -0300

    Livros com table

--- WebLivraria/src/app/models/Livro.ts
+++ WebLivraria/src/app/models/Livro.ts
@@ -1,10 +1,11 @@
 export default interface Livro {
         id: string;
         title: string;
+        subtitle: string;
         resume: string
         gender: string;
         pages: number;
-        date: string;
+        year: string;
         edition: number;
         nameAuthor: string;
         namePublisher: string;
--- WebLivraria/src/app/views/livros/livros.component.ts
+++ WebLivraria/src/app/views/livros/livros.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-livros',
   templateUrl: './livros.component.html',
   styleUrls: ['./livros.component.css']
 })
-export class LivrosComponent {
+export class LivrosComponent implements OnInit {
+  livros: LivrosComponent[] = []
+  displayedColumns: string[] = ['title', 'subtitle', 'id', 'gender', 'pages', 'year', 'edition', 'author', 'publisher', 'resume'];
 
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
 }
